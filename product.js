5df3c7dc4766f746a96d193acb4a7e7706607ad2
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-03-10 17:52:47 +0000

    done

---
+++ product.js
@@ -0,0 +1,7 @@
+const Products = [
+    {id: 1, name: "🍎Apple", price:100},
+    {id: 2, name: "🍇Grapes", price:200},
+    {id: 3, name: "🍌Banana", price:300},
+    {id: 4, name: "🍊Orange", price:500},
+    ];
+    
